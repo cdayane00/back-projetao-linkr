fa2987829c9b15577ab781cc84472cc944dfb209
Arthur Akira <arthurak10.at@gmail.com>
2022-08-11 14:58:08 -0300

    small changes

--- src/routes/hashtagsRoute.js
+++ src/routes/hashtagsRoute.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
+
 import {
   getPostsByHashtagName,
   listHashtags,
 } from "../controllers/hashtagController.js";
+
 import { checkIfHashtagExists } from "../middlewares/hashtagMiddlewares.js";
+import { tokenAuth } from "../middlewares/tokenMiddleware.js";
 
 export const hashtagsRouter = Router();
 
-hashtagsRouter.get("/hashtags", listHashtags);
+hashtagsRouter.get("/hashtags", tokenAuth, listHashtags);
 
 hashtagsRouter.get(
   "/hashtags/:hashtag",
+  tokenAuth,
   checkIfHashtagExists,
   getPostsByHashtagName
 );
